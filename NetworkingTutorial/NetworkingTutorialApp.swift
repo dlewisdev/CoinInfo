e64df202c55a85d2e0320106913aaa35d2c81f5d
Danielle Lewis <msdaniellelewis@icloud.com>
2023-10-25 23:12:11 -0400

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Danielle Lewis on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
