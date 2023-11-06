bb2152ccc48890960db75812224e5f31271596b0
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-06 17:50:41 -0500

    Adds image fetching capability to project.

--- NetworkingTutorial/Shared/Images/CoinImageView.swift
+++ NetworkingTutorial/Shared/Images/CoinImageView.swift
@@ -6,13 +6,22 @@
 //
 
 import SwiftUI
+//Third party service KingFisher can load images and cache them
 
 struct CoinImageView: View {
+    @ObservedObject var imageLoader: ImageLoader
+    
+    init(url: String) {
+        imageLoader = ImageLoader(url: url)
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let image = imageLoader.image {
+            image
+                .resizable()
+        }
     }
 }
 
 #Preview {
-    CoinImageView()
+    CoinImageView(url: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400")
 }
--- NetworkingTutorial/Shared/Images/ImageLoader.swift
+++ NetworkingTutorial/Shared/Images/ImageLoader.swift
@@ -5,4 +5,27 @@
 //  Created by Danielle Lewis on 11/6/23.
 //
 
-import Foundation
+import SwiftUI
+
+class ImageLoader: ObservableObject {
+    @Published var image: Image?
+    
+    private let urlString: String
+    
+    init(url: String) {
+        self.urlString = url
+        Task { await loadImage() }
+    }
+    
+    @MainActor
+    private func loadImage() async {
+        guard let url = URL(string: urlString) else { return }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            guard let uiImage = UIImage(data: data) else { return }
+            self.image = Image(uiImage: uiImage)
+        } catch {
+            print("DEBUG: failed to fetch image with error \(error)")
+        }
+    }
+}
