0082e0ccef5f2cf369374bee55e53b98f9b8f64a
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-06 17:51:41 -0500

    Interpolates page value and per_page for pagination functionality.

--- NetworkingTutorial/Core/Service/CoinDataService.swift
+++ NetworkingTutorial/Core/Service/CoinDataService.swift
@@ -14,11 +14,12 @@ protocol CoinServiceProtocol {
 
 class CoinDataService: CoinServiceProtocol, HTTPDataDownloader {
     
-    init() {
-        print("DEBUG: Did init service...")
-    }
+    private var page: Int = 0
+    private let fetchLimit = 30
     
     func fetchCoins() async throws -> [Coin] {
+        page += 1
+        
         guard let endpoint = allCoinsURLString else {
             throw CoinAPIError.requestFailed(description: "Invalid endpoint")
         }
@@ -55,8 +56,8 @@ class CoinDataService: CoinServiceProtocol, HTTPDataDownloader {
         components.queryItems = [
             .init(name: "vs_currency", value: "usd"),
             .init(name: "order", value: "market_cap_desc"),
-            .init(name: "per_page", value: "20"),
-            .init(name: "page", value: "1"),
+            .init(name: "per_page", value: "\(fetchLimit)"),
+            .init(name: "page", value: "\(page)"),
             .init(name: "price_change_percentage", value: "24h")
         ]
         
