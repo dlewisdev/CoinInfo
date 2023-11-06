ecf751a2e041329a9ca357ba3e760aea49bda05a
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-06 17:51:17 -0500

    Adds mock service capability to save API calls.

--- NetworkingTutorial/Core/Service/MockCoinService.swift
+++ NetworkingTutorial/Core/Service/MockCoinService.swift
@@ -8,13 +8,29 @@
 import Foundation
 
 class MockCoinService: CoinServiceProtocol {
+    
+    var mockData: Data?
+    var mockError: CoinAPIError?
+    
     func fetchCoins() async throws -> [Coin] {
-        return []
+        if let mockError { throw mockError }
+        
+        do {
+            let coins = try JSONDecoder().decode([Coin].self, from: mockData ?? mockCoinData_marketCapDesc)
+            return coins
+        } catch {
+            throw error as? CoinAPIError ?? .unknownError(error: error)
+        }
     }
     
     func fetchCoinDetails(id: String) async throws -> CoinDetails? {
-        return nil
+        let description = Description(text: "Test description")
+       let bitcoinDetails = CoinDetails(id: "bitcoin", symbol: "btc", name: "Bitocin", description: description)
+        return bitcoinDetails
     }
     
+    func doSomething() {
+        print("Do stuff")
+    }
     
 }
